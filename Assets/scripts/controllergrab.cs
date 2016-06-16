ebbdb65527513eef451bf56e9bc80ee6b90e437c
TaterHater <nick_beeson@yahoo.com>
2016-06-15 23:14:59 -0600

    fixed grabbing

--- Assets/scripts/controllergrab.cs
+++ Assets/scripts/controllergrab.cs
@@ -1,46 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class controllergrab : MonoBehaviour {
+public class controllergrab : MonoBehaviour
+{
+
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    public bool gripButtonDown = false;
+    public bool gripButtonUp = false;
+    public bool gripButtonPressed = false;
+
+    private GameObject tool;
 
     private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
     public bool triggerButtonDown = false;
     public bool triggerButtonUp = false;
     public bool triggerButtonPressed = false;
     public GameObject cntrller;
+    private Collider C;
     private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
     private SteamVR_TrackedObject trackedObj;
-    void Start () {
+    void Start()
+    {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         triggerButtonDown = controller.GetPressDown(triggerButton);
         triggerButtonUp = controller.GetPressUp(triggerButton);
         triggerButtonPressed = controller.GetPress(triggerButton);
+        gripButtonDown = controller.GetPressDown(gripButton);
+        gripButtonUp = controller.GetPressUp(gripButton);
+        gripButtonPressed = controller.GetPress(gripButton);
         if (triggerButtonDown)
         {
             Debug.Log("trigger down!");
         }
+        if (gripButtonDown)
+        {
+            Debug.Log("grip down!");
+            if (C != null)
+            {
+                C.transform.SetParent(null);
+                C.GetComponent<Rigidbody>().isKinematic = false;
+                C = null;
+            }
         }
+    }
     void OnTriggerEnter(Collider c)
     {
-        Debug.Log("collider down!"+c.tag);
+        Debug.Log("collider down!" + c.tag);
         if (triggerButtonPressed)
         {
             Debug.Log("down in collider" + c.tag);
-
-            c.gameObject.transform.SetParent(cntrller.transform);
+            tool = c.GetComponent<GameObject>();
+            c.transform.SetParent(cntrller.transform);
             c.GetComponent<Rigidbody>().isKinematic = true;
+            C = c;
+
             c.gameObject.transform.Translate(new Vector3(0, 0, 0));
+            //tool = null;
 
         }
-        else
-        {
-            c.gameObject.transform.SetParent(null);
-            c.GetComponent<Rigidbody>().isKinematic = false;
 
-        }
     }
 }
