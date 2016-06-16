f29f0beacbd908945c49e9f16f970ebd4f7ada90
TaterHater <nick_beeson@yahoo.com>
2016-06-15 22:57:53 -0600

    gripy stuff, changes in main

---
+++ Assets/scripts/controllergrab.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controllergrab : MonoBehaviour {
+
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    public bool triggerButtonDown = false;
+    public bool triggerButtonUp = false;
+    public bool triggerButtonPressed = false;
+    public GameObject cntrller;
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+    void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        triggerButtonDown = controller.GetPressDown(triggerButton);
+        triggerButtonUp = controller.GetPressUp(triggerButton);
+        triggerButtonPressed = controller.GetPress(triggerButton);
+        if (triggerButtonDown)
+        {
+            Debug.Log("trigger down!");
+        }
+        }
+    void OnTriggerEnter(Collider c)
+    {
+        Debug.Log("collider down!"+c.tag);
+        if (triggerButtonPressed)
+        {
+            Debug.Log("down in collider" + c.tag);
+
+            c.gameObject.transform.SetParent(cntrller.transform);
+            c.GetComponent<Rigidbody>().isKinematic = true;
+            c.gameObject.transform.Translate(new Vector3(0, 0, 0));
+
+        }
+        else
+        {
+            c.gameObject.transform.SetParent(null);
+            c.GetComponent<Rigidbody>().isKinematic = false;
+
+        }
+    }
+}
---
+++ Assets/scripts/fieldSpawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fieldSpawn : MonoBehaviour {
+    public int size;
+    public GameObject tile;
+    public int[,] filedArr = new int[25,25]; 
+	void Start () {
+	for(int i = 0; i < size; i++)
+        {
+            for(int j = 0; j < size; j++)
+            {
+                Instantiate(tile, new Vector3(100-i * 5, 0, 100-j * 5),Quaternion.identity);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
